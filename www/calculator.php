3293b2a30e5b339847222a73feb4514bb93c487f
LAMP <roxas.johnlloydb@gmail.com>
2024-11-06 15:42:22 +0800

    Set empty file

---
+++ www/calculator.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>LAMP STACK</title>
+        <link rel="shortcut icon" href="/assets/images/favicon.svg" type="image/svg+xml">
+        <link rel="stylesheet" href="/assets/css/bulma.min.css">
+    </head>
+    <body>
+       
+    </body>
+</html>
